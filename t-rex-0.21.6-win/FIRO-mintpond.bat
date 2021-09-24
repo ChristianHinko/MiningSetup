2903e1c51701ac5a322aa527add9b2bc05a7ec69
include-iostream <c4hinkle@gmail.com>
2021-09-24 01:25:46 -0300

    update to t-rex 0.22.1

--- t-rex-0.21.6-win/FIRO-mintpond.bat
+++
@@ -1,2 +0,0 @@
-t-rex.exe -a mtp -o stratum+ssl://firo.mintpond.com:3005 -u aBR3GY8eBKvEwjrVgNgSWZsteJPpFDqm6U.rig0 -p x
-pause
--- t-rex-0.21.6-win/RVN-2miners.bat
+++
@@ -1,2 +0,0 @@
-t-rex.exe -a kawpow -o stratum+tcp://us-rvn.2miners.com:6060 -u RTCKF9nAWVRKWdBhFtRzzWqDy7XcsJx48p.PersonalDesktop -p x --intensity 25
-#pause
