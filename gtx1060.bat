bf02a3241a2e8953df98774fb337f3ec5b8cf6e2
include-iostream <c4hinkle@gmail.com>
2021-09-24 01:17:28 -0300

    update gtx1060 bat to use ShardPool for ZIL

--- gtx1060.bat
+++ gtx1060.bat
@@ -1,2 +1,2 @@
-"t-rex-0.21.6-win\t-rex.exe" -a ethash -o stratum+tcp://us1.ethermine.org:4444 -u 0xa40bEF4844156fB5619d279Facb11F595Fbe0853 -p x -w PersonalDesktop --intensity 25 -d 1
+"t-rex-0.21.6-win\t-rex.exe" -a ethash -o stratum+tcp://us1-zil.shardpool.io:3333 -u 0xa40bEF4844156fB5619d279Facb11F595Fbe0853 -p zil1sqmtypec6k2rmqshwzwjcwc83snp29cyqmhgaq@us1.ethermine.org:4444 -w PersonalDesktop --intensity 25 -d 1
 #pause
