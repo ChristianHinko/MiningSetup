8ce7c1ee35d4e4ba93dc8d09632af981d8baa921
include-iostream <c4hinkle@gmail.com>
2021-10-06 21:55:07 -0700

    fix bat files to use the new updated miner paths

--- gtx1060.bat
+++ gtx1060.bat
@@ -1,2 +1,2 @@
-"t-rex-0.22.1-win\t-rex.exe" -a ethash -o stratum+tcp://us1-zil.shardpool.io:3333 -u 0xe5Fc436ED645e8B1fD1dc8E4D50156F67D866Ac8 -p zil1sqmtypec6k2rmqshwzwjcwc83snp29cyqmhgaq@us1.ethermine.org:4444 -w PersonalDesktop --intensity 25 -d 1
+"t-rex-0.23.2-win\t-rex.exe" -a ethash -o stratum+tcp://us1-zil.shardpool.io:3333 -u 0xe5Fc436ED645e8B1fD1dc8E4D50156F67D866Ac8 -p zil1sqmtypec6k2rmqshwzwjcwc83snp29cyqmhgaq@us1.ethermine.org:4444 -w PersonalDesktop --intensity 25 -d 1
 #pause
