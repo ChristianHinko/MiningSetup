a0a820331fb15684c9f75795b8deac057745aa4f
include-iostream <c4hinkle@gmail.com>
2021-10-29 01:21:09 -0300

    make RemoteRestart dotnet api for restarting rigs remotely

---
+++ RemoteRestart/RemoteRestart/Controllers/RestartController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemoteRestart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestartController : ControllerBase
+    {
+        public RestartController()
+        {
+
+        }
+
+        [HttpGet]
+        public void RestartMachine()
+        {
+            // Restart this machine
+            Process.Start("shutdown.exe", "-r -t 0");
+        }
+
+    }
+}
