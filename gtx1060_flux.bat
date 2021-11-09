918c84e1649754b6f95418b76ee4837c29af781d
include-iostream <c4hinkle@gmail.com>
2021-11-09 14:40:52 -0400

    fix rtx3060_eth to specify devices 0

--- gtx1060_flux.bat
+++ gtx1060_flux.bat
@@ -1,2 +1,2 @@
-"gminer_2_70_windows64/miner.exe" --algo 125_4 --server us-flux.fluxpools.net:2001 --user t1T9FBBuRmrcWwjZHqW5KXxABDqbb8TTXwb.PersonalDesktop1 --pass x --pers ZelProof --devices 1
+"gminer_2_70_windows64/miner.exe" --algo 125_4 --server us-flux.fluxpools.net:2001 --user t1T9FBBuRmrcWwjZHqW5KXxABDqbb8TTXwb.PersonalDesktop1 --pass x --pers ZelProof --intensity 100 --devices 1
 #pause
--- rtx3060_eth.bat
+++ rtx3060_eth.bat
@@ -1,2 +1,2 @@
-"NBMiner_Win/nbminer" --algo ethash --url stratum+tcp://us1.ethermine.org:4444 --user 0xe5Fc436ED645e8B1fD1dc8E4D50156F67D866Ac8.PersonalDesktop0 --intensity 100
+"NBMiner_Win/nbminer" --algo ethash --url stratum+tcp://us1.ethermine.org:4444 --user 0xe5Fc436ED645e8B1fD1dc8E4D50156F67D866Ac8.PersonalDesktop0 --intensity 100 --devices 0
 #pause
