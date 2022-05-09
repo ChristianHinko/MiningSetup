67c651bad74de034389203fca00d29f5b03ef71e
Christian Hinkle <c4hinkle@gmail.com>
2022-05-08 22:35:33 -0400

    Add schtasks xml file for running applications as administrator

--- PersonalDesktop0_eth.bat
+++ PersonalDesktop0_eth.bat
@@ -1,2 +1,2 @@
-"NBMiner_Win/nbminer" --algo ethash --url stratum+tcp://us1-zil.shardpool.io:3333 --user 0xe5Fc436ED645e8B1fD1dc8E4D50156F67D866Ac8.PersonalDesktop0 --password zil1sqmtypec6k2rmqshwzwjcwc83snp29cyqmhgaq@us1.ethermine.org:4444 --intensity 100 --devices 0
+"NBMiner_Win/nbminer.exe" --algo ethash --url stratum+tcp://us1-zil.shardpool.io:3333 --user 0xe5Fc436ED645e8B1fD1dc8E4D50156F67D866Ac8.PersonalDesktop0 --password zil1sqmtypec6k2rmqshwzwjcwc83snp29cyqmhgaq@us1.ethermine.org:4444 --intensity 100 --devices 0
 #pause
