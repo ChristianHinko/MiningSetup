a3b3252f622699b40841ffecece55f48ea06c29d
Christian Hinkle <c4hinkle@gmail.com>
2022-05-11 23:25:13 -0400

    make FxBoi0 and FxBoi1 files and clean up other stuff (these were old changes that I am now commiting)

---
+++ FxBoi0_erg.bat
@@ -0,0 +1,7 @@
+set GPU_MAX_ALLOC_PERCENT=100
+set GPU_SINGLE_ALLOC_PERCENT=100
+set GPU_MAX_HEAP_SIZE=100
+set GPU_USE_SYNC_OBJECTS=1
+
+"teamredminer-v0.9.0-win/teamredminer.exe" --algo autolykos2 --url stratum+tcp://us.ergo.herominers.com:10250 --user 9fc314ndVamV3dcBQ38nBN2WzcgeTeyeC9MzQfu7WmFHf4V3XK7.FxBoi0 --pass x --eth_config=B768 --temp_limit=90 --zil --url stratum+tcp://us1-zil.shardpool.io:3333 --user 0xe5Fc436ED645e8B1fD1dc8E4D50156F67D866Ac8.FxBoi --pass zil1sqmtypec6k2rmqshwzwjcwc83snp29cyqmhgaq@us1.ethermine.org:4444 --eth_config=B768 --temp_limit=90 --zil_end
+#pause
---
+++ FxBoi1_eth.bat
@@ -0,0 +1,2 @@
+"t-rex-0.24.8-win/t-rex.exe" --algo ethash --url stratum+tcp://us1-zil.shardpool.io:3333 --user 0xe5Fc436ED645e8B1fD1dc8E4D50156F67D866Ac8.FxBoi1 --pass zil1sqmtypec6k2rmqshwzwjcwc83snp29cyqmhgaq@us1.ethermine.org:4444 --intensity 25 --devices 0
+#pause
